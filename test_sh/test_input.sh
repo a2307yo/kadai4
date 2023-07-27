fb5f65fa47c7bcc9e4d850d1a9ed88bd0a1883d0
Yuta Ozawa <a2307yo@aiit.ac.jp>
2023-07-27 16:42:10 +0900

    Add test Script "test_input.sh"

--- test_sh/test_input.sh
+++ test_sh/test_input.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-echo "test_case1";
-./gcd.sh; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh 1; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh a; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh 1 a; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh a 1; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh 0 1; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh 1 0; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-./gcd.sh 0.1 2; [ "$?" -eq 1 ] && echo "PASS" || echo "FAIL"
-
-echo "test_case2";
-[ "$(./gcd.sh 1 1)" -eq 1 ] && echo "PASS" || echo "FAIL"
-[ "$(./gcd.sh 6 2)" -eq 2 ] && echo "PASS" || echo "FAIL"
-[ "$(./gcd.sh 2 6)" -eq 2 ] && echo "PASS" || echo "FAIL"
-[ "$(./gcd.sh 6 3)" -eq 3 ] && echo "PASS" || echo "FAIL"
-[ "$(./gcd.sh 3 6)" -eq 3 ] && echo "PASS" || echo "FAIL"
+echo "input check";
+./gcd.sh;       [ "$?" -eq 1 ] && echo "test case1 PASS" || $(echo "test case1 FAIL"; exit 1)
+./gcd.sh 1;     [ "$?" -eq 1 ] && echo "test case2 PASS" || $(echo "test case2 FAIL"; exit 1)
+./gcd.sh a;     [ "$?" -eq 1 ] && echo "test case3 PASS" || $(echo "test case3 FAIL"; exit 1)
+./gcd.sh 1 a;   [ "$?" -eq 1 ] && echo "test case4 PASS" || $(echo "test case4 FAIL"; exit 1)
+./gcd.sh a 1;   [ "$?" -eq 1 ] && echo "test case5 PASS" || $(echo "test case5 FAIL"; exit 1)
+./gcd.sh 0 1;   [ "$?" -eq 1 ] && echo "test case6 PASS" || $(echo "test case6 FAIL"; exit 1)
+./gcd.sh 1 0;   [ "$?" -eq 1 ] && echo "test case7 PASS" || $(echo "test case7 FAIL"; exit 1)
+./gcd.sh 0.1 2; [ "$?" -eq 1 ] && echo "test case8 PASS" || $(echo "test case8 FAIL"; exit 1)
+./gcd.sh 5 -2; [ "$?" -eq 1 ] && echo "test case8 PASS" || $(echo "test case8 FAIL"; exit 1)
